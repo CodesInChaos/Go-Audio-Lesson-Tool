b72ca99a203a76498e121454cd153a63445d362d
CodeInChaos <nobody@example.com>
2010-07-13 15:27:00 +0200

    Moved Xiph to external repo

--- Xiph/Convert/all.h
+++
@@ -1,4 +0,0 @@
-#include "ogg/ogg.h"
-#include "vorbis/codec.h"
-#include "vorbis/vorbisfile.h"
-#include "vorbis/vorbisenc.h"
--- Xiph/Convert/header.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using System.Runtime.InteropServices;
-
-namespace Xiph.LowLevel
-{
